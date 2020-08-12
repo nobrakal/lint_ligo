4e4163691e59c10748fbb1e564e2f2851c72c793
Alexandre Moine <alexandre@moine.me>
2020-08-12 13:29:19 +0200

    use mini_c

--- lib/main.ml
+++ lib/main.ml
@@ -15,7 +15,7 @@ let parse_rules buf =
   Parser.rules Lexer.token buf
 
 let parse_ast str_ast =
-  Ast_imperative.program_of_yojson (Yojson.Safe.from_string str_ast)
+  Mini_c.program_of_yojson (Yojson.Safe.from_string str_ast)
 
 let serialize result =
   let yojson_result = `List (List.map Simple_utils.Trace.annotation_to_yojson result) in
